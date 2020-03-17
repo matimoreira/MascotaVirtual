568a8b5350dcce22547b10466771acf8c1e758f8
matimoreira <58670479+matimoreira@users.noreply.github.com>
2020-03-17 12:31:50 -0300

    Agregar archivos de proyecto.

---
+++ MascotaVirtual.Test/MascotaTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MascotaVirtual;
+using Xunit;
+
+namespace MascotaVirtual.Test
+{
+    public class MascotaTest
+    {
+        [Fact]
+        public void Comer_ComioAntesYNoFueAlBaño_DeberiaArrojarExcepcion()
+        {
+            // Arrange
+            var mascota = new Mascota();
+
+            // Act
+            mascota.Comer();
+
+
+            // Assert
+            Assert.Throws<Exception>(() => mascota.Comer());
+        }
+        [Fact]
+        public void Comer_ComioAntesYFueAlBaño_DeberiaArrojarComiendo()
+        {
+            // Arrange
+            var mascota = new Mascota();
+
+            // Act
+            mascota.Comer();
+            mascota.IrAlBaño();
+
+            mascota.Comer();
+
+            // Assert
+            Assert.Equal("Comiendo", mascota.Estado);
+        }
+
+        [Fact]
+        public void RealizarUnaAccion_Dormido_DeberiaArrojarExcepcionEnTodasMenosEnDespertarse()
+        {
+            // Arrange
+            var mascota = new Mascota();
+
+            // Act
+            mascota.Dormir();
+            
+
+            // Assert
+            Assert.Throws<Exception>(() => mascota.Comer());
+            Assert.Throws<Exception>(() => mascota.IrAlBaño());
+            Assert.Throws<Exception>(() => mascota.Jugar());
+            Assert.Throws<Exception>(() => mascota.Dormir());
+
+            mascota.Despertar();
+            
+            Assert.Equal("Despertando", mascota.Estado);
+        }
+
+        [Fact]
+        public void Despertarse_NoEstaDurmiendo_DeberiaArrojarExcepcion()
+        {
+            // Arrange
+            var mascota = new Mascota();
+
+            // Act
+
+            // Assert
+            Assert.Throws<Exception>(() => mascota.Despertar());
+        }
+
+        [Fact]
+        public void Jugar_SinHaberComidoAntes_DeberiaArrojarExcepcion()
+        {
+            // Arrange
+            var mascota = new Mascota();
+
+            // Act
+
+            // Assert
+            Assert.Throws<Exception>(() => mascota.Jugar());
+        }
+
+        [Fact]
+        public void Jugar_ComidoAntes_DeberiaArrojarJugando()
+        {
+            // Arrange
+            var mascota = new Mascota();
+
+            // Act
+            mascota.Comer();
+            mascota.Jugar();
+
+            // Assert
+            Assert.Equal("Jugando", mascota.Estado);
+        }
+        [Fact]
+        public void IrAlBaño_SinHaberComido_DeberiaArrojarExcepcion()
+        {
+            // Arrange
+            var mascota = new Mascota();
+
+            // Act
+
+            // Assert
+            Assert.Throws<Exception>(() => mascota.IrAlBaño());
+        }
+
+    }
+}
---
+++ MascotaVirtual/Mascota.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MascotaVirtual
+{
+    public class Mascota
+    {
+        private bool _apetito;
+        public Mascota()
+        {
+            _apetito = true;
+            this.Name = "Coca";
+        }
+        public Mascota(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+        public string Estado { get; set; }
+        public void Comer()
+        {
+            if (!_apetito)
+            {
+                throw new Exception($"{this.Name} no tiene hambre.");
+            }
+            throwIfState("Durmiendo", $"{this.Name} esta durmiendo.");
+            this.Estado = "Comiendo";
+            _apetito = false;
+        }
+
+        public void Jugar()
+        {
+            if (_apetito)
+            {
+                throw new Exception($"{this.Name} no tiene energias para jugar.");
+            }
+            throwIfState("Durmiendo", $"{this.Name} esta durmiendo.");
+            this.Estado = "Jugando";
+            _apetito = true;
+        }
+
+        public void Dormir()
+        {
+            throwIfState("Durmiendo", $"{this.Name} ya esta durmiendo.");
+            this.Estado = "Durmiendo";
+        }
+
+        public void Despertar()
+        {
+            if (this.Estado != "Durmiendo")
+            {
+                throw new Exception($"{this.Name} ya esta Despierto.");
+            }
+            this.Estado = "Despertando";
+        }
+
+        public void IrAlBaño()
+        {
+            if (_apetito)
+            {
+                throw new Exception($"{this.Name} tiene una lija, que va tener ganas de ir al baño.");
+            }
+            throwIfState("Durmiendo", $"{this.Name} esta durmiendo.");
+            this.Estado = "***ando";
+            this._apetito = true;
+        }
+
+        public void throwIfState(string state, string message)
+        {
+            if (this.Estado == state)
+            {
+                throw new Exception(message);
+            }
+        }
+    }
+}
